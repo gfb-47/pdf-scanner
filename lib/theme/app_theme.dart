15196ca6a9070f0390b4e62c2cb7a833500e286a
Anotherafael <rafael.afmendonca1994@gmail.com>
2022-06-08 16:02:46 -0300

    🛠 Theme and components added to shared paste.

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,14 +1,1 @@
-import 'package:flutter/material.dart';
-
-import 'app_colors.dart';
-
-class AppTheme {
-  static ThemeData get defaultTheme => ThemeData(
-        scaffoldBackgroundColor: AppColors.whiteAlt,
-        appBarTheme: AppBarTheme(
-          backgroundColor: AppColors.primary,
-          elevation: 0,
-          centerTitle: true,
-        ),
-      );
-}
+// TODO Implement this library.
