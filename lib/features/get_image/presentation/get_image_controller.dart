c2a307c16960ccaed9a8fd3c62ba19d6cbb5d45d
Anotherafael <rafael.afmendonca1994@gmail.com>
2022-06-08 16:33:58 -0300

    🛠 showDialog added on failures

--- lib/features/get_image/presentation/get_image_controller.dart
+++ lib/features/get_image/presentation/get_image_controller.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
+import '../../../shared/components/dialog/error_dialog.dart';
 import '../data/datasources/camera_data_source.dart';
 import '../data/datasources/gallery_data_source.dart';
 import '../data/repositories/get_image_repository_impl.dart';
@@ -20,7 +21,11 @@ class GetImageController {
     final response = await repository.takePicture();
     response.fold(
       (l) {
-        print('Probleminha');
+        ErrorDialog.showErrorDialog(
+          context,
+          'Failure on Taking Picture',
+          'Sorry for the failure, please try again or send us a report.',
+        );
       },
       (image) {
         this.image = File(image.imagePath);
@@ -37,7 +42,11 @@ class GetImageController {
     final response = await repository.pickImage();
     response.fold(
       (l) {
-        print('Probleminha');
+        ErrorDialog.showErrorDialog(
+          context,
+          'Failure on Picking Image from Gallery',
+          'Sorry for the failure, please try again or send us a report.',
+        );
       },
       (image) {
         this.image = File(image.imagePath);
