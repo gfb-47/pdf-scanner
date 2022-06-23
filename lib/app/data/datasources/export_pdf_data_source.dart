28f4170ffe77b9144d49225880c2d0c4b560bc41
Gabriel Beckman <gabrieelfbrito@gmail.com>
2022-06-23 01:03:13 -0300

    Open pdf, remove botao, corrige multidex

--- lib/app/data/datasources/export_pdf_data_source.dart
+++ lib/app/data/datasources/export_pdf_data_source.dart
@@ -1,10 +1,9 @@
 // ignore_for_file: invalid_use_of_visible_for_testing_member
 
+import 'dart:developer';
 import 'dart:io';
 
-import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
-import 'package:open_file_safe/open_file_safe.dart' as of;
+import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
@@ -13,9 +12,9 @@ import '../../domain/core/error/failures.dart';
 import '../../domain/entities/image_entity.dart';
 
 abstract class IExportPdfDatasource {
-  Future<void> exportPdf(ImageEntity image, BuildContext context);
+  Future<void> exportPdf(ImageEntity image);
   Future<void> createPdf(ImageEntity image);
-  Future<void> savePdf(BuildContext context);
+  Future<void> savePdf();
 }
 
 class ExportPdfDatasource implements IExportPdfDatasource {
@@ -24,10 +23,10 @@ class ExportPdfDatasource implements IExportPdfDatasource {
   final pdf = pw.Document();
 
   @override
-  Future<void> exportPdf(ImageEntity image, BuildContext context) async {
+  Future<void> exportPdf(ImageEntity image) async {
     await createPdf(image);
     // ignore: use_build_context_synchronously
-    await savePdf(context);
+    await savePdf();
   }
 
   @override
@@ -52,18 +51,17 @@ class ExportPdfDatasource implements IExportPdfDatasource {
   }
 
   @override
-  Future<void> savePdf(BuildContext context) async {
+  Future<void> savePdf() async {
     try {
-<<<<<<< HEAD
-      final dir = await getExternalStorageDirectory();
+      final dir = Platform.isAndroid
+          ? await getExternalStorageDirectory()
+          : await getApplicationDocumentsDirectory();
       final fileDir = '${dir!.path}/${DateTime.now()}.pdf';
       final file = File(fileDir);
-=======
-      final dir = await getApplicationSupportDirectory();
-      final file = File('${dir.path}/file.pdf');
->>>>>>> e429c54bad9194a222b7ade443c68985f576271a
       await file.writeAsBytes(await pdf.save());
-      // ignore: avoid_dynamic_calls
+      final url = file.path;
+      await OpenFile.open(url);
+      log(url);
     } catch (e) {
       throw ExportPdfDatasourceFailure();
     }
